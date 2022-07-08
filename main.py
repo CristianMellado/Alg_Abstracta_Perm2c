e0ce417a84695519ca9879306d12141ca2073fd6
CristianMellado <85757675+CristianMellado@users.noreply.github.com>
2022-07-08 00:00:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from ataque_rsa_1 import attack1
+from ataque_rsa_2 import attack2
+from ataque_rsa_3 import attack3
+
+def main():
+    print("\n----------- ATTACK 1 -----------\n")
+    attack1()
+
+    print("\n----------- ATTACK 2 -----------\n")
+    attack2()
+    
+    print("\n----------- ATTACK 3 -----------\n")
+    attack3()
+
+main()
