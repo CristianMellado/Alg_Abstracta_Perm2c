846d70085dae5c7abbba1c97c4e4aba6bcfa3ab3
CristianMellado <85757675+CristianMellado@users.noreply.github.com>
2022-07-08 00:29:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from ataque_rsa_2 import attack2
 from ataque_rsa_3 import attack3
 
 def main():
-    print("\n----------- ATTACK 1 -----------\n")
+    print("\n-------- ATAQUE 1 --------\n")
     attack1()
 
-    print("\n----------- ATTACK 2 -----------\n")
+    print("\n-------- ATAQUE 2 --------\n")
     attack2()
     
-    print("\n----------- ATTACK 3 -----------\n")
+    print("\n-------- ATAQUE 3 --------\n")
     attack3()
 
 main()
